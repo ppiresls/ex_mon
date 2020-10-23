4518b7d3999b6e0640727acbc1122bd9216dee87
Patrick Pires <patrick.lourenco@globalhitss.com.br>
2020-10-23 15:57:59 -0300

    feat: printing game status

---
+++ lib/game/status.ex
@@ -0,0 +1,9 @@
+defmodule ExMon.Game.Status do
+  alias ExMon.Game
+
+  def print_round_message() do
+    IO.puts("\n------------ Game has started! ------------\n")
+    IO.inspect(Game.info)
+    IO.puts("-------------------------------------------")
+  end
+end
